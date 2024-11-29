18838d90ca89c380a45a2d1d50863f26c5d133f0
JoseMa Esparev <esparev@hotmail.com>
2024-11-28 23:15:48 -0600

    feat: ✨ add Inter font and apply it to the body element
    - Import Inter font from 'next/font/google'
    - Define the 'inter' variable with weight, style, subsets, display, and variable properties
    - Apply the 'inter' variable to the body element in the RootLayout component
    - Add 'font-sans' class to the base layer in globals.css
    - Extend the theme in tailwind.config.ts to include 'var(--font-inter)' in the 'sans' font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
+// Libs
 import type { Metadata } from 'next';
+// Local Libs
+import { inter } from '@app/app/fonts';
+// Utils
+import { cn } from '@app/utils/cn';
+// Styles
 import '@app/assets/styles/globals.css';
 
 export const metadata: Metadata = {
@@ -9,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="es-mx">
-      <body>{children}</body>
+      <body className={cn('flex min-h-screen flex-col', inter.variable)}>{children}</body>
     </html>
   );
 }
